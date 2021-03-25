751e382ed710a82a1d56c23ce10d0690b2cf9d02
SexBoyy <81386475+SexBoyy@users.noreply.github.com>
2021-03-25 22:46:12 +0300

    Delete src directory

--- src/task7/Distance.java
+++
@@ -1,9 +0,0 @@
-package task7;
-
-import java.awt.*;
-
-public class Distance {
-    public static double calcDistanceToXY(Point point) {
-        return Math.sqrt(1.0 * point.x * point.x + point.y * point.y);
-    }
-}
