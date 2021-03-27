7afe5587d001d96fbb2a01e2555b9d06b95cab83
SexBoyy <81386475+SexBoyy@users.noreply.github.com>
2021-03-27 11:01:40 +0300

    Delete Lab_1 directory

--- Lab_1/src/task1/Number.java
+++
@@ -1,8 +0,0 @@
-package task1;
-
-public class Number {
-    public static int getLastSquareNumber(int number){
-        int lastNumber = number%10;
-        return (lastNumber*lastNumber)%10;
-    }
-}
--- Lab_1/src/task3/SquareArea.java
+++
@@ -1,8 +0,0 @@
-package task3;
-
-public class SquareArea {
-    public static double calcSquareArea(double area) throws IllegalArgumentException{
-        if (area<0) throw new IllegalArgumentException("Площадь не может быть отрицательной");
-        return area/2;
-    }
-}
