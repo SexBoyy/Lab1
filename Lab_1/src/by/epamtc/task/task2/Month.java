c404eb7155037b4aa0c772e6ed2c7c3e1b10e038
SexBoyy <81386475+SexBoyy@users.noreply.github.com>
2021-04-17 16:09:47 +0300

    Add files via upload

---
+++ Lab_1/src/by/epamtc/task/task2/Month.java
@@ -0,0 +1,16 @@
+package by.epamtc.task.task2;
+
+public enum Month {
+    JANUARY,
+    FEBRUARY,
+    MARCH,
+    APRIL,
+    MAY,
+    JUNE,
+    JULY,
+    AUGUST,
+    SEPTEMBER,
+    OCTOBER,
+    NOVEMBER,
+    DECEMBER
+}
